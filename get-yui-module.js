ea20178df31117ecf1a97ad450c216ce7433b1fa
Joseph Chiang <joseph_chiang@miiicasa.com>
2012-09-20 14:38:26 +0800

    Fix with JSLint

--- get-yui-module.js
+++ get-yui-module.js
@@ -1,85 +1,143 @@
 #!/usr/bin/env node
-
-// Make an anonymous function so that I can stop execution.
+/**
+ * YUI Config Collector
+ *
+ * The purpose of this script is to collect module settings
+ * of customized YUI module files. By doing this, we don't have to maintain
+ * a centralized module dependencies config. Instead we can write it in
+ * individual modules.
+ *
+ * Usage:
+ * ````
+ * ycc <path> --ignore <keyword> --output <output>
+ * ````
+ */
+/*global YUI,GLOBAL */
+// Make an anonymous function so that I can stop execution anytime.
 (function () {
 
-    var exec   = require("child_process").exec,
+    var exec,
         argv,
-        fs     = require("fs"),
-        output = {},
+        fs,
+        cmd,
+        i,
         path,
-        child,
-        currentFile;
+        ignore, // The ignore keyword list.
+        modules, // The output config.
+        currentFile, // Current file path.
+        sortObject;
 
+    // Define command-line options.
     argv = require("optimist")
-        .usage("\nCollect configuration of each customized YUI modules.\n\n" +
-               "Usage: get-yui-module.js --path [Path]")
-        .demand("path")
-        .describe("path", "The path you want to find customized YUI modules.")
-        .describe("output", "The output path.")
-        .argv;
+           .usage("Collect configuration of each customized YUI modules.\n\n" +
+                  "The purpose of this script is to collect module settings" +
+                  "of customized YUI module files.\nBy doing this, we don't " +
+                  "have to maintain a centralized module dependencies config.\n\n" +
+                  "Usage: ycc <Search Path> --ignore " +
+                  "[Ignore keyword List] --output [Output File]")
+           .describe("ignore", "You can ignore files by providing keyword list " +
+                     "which separate by common")
+           .describe("output", "Output the result to a file.")
+           .check(function(o) {
+               // The path must exist.
+               if (!o._[0]) {
+                   throw new Error("You must at least provide path.");
+               }
+               if (!require("path").existsSync(o._[0])) {
+                   throw new Error("The path you provide doesn't exist.");
+               }
+           })
+           .argv;
 
+    path = argv._[0];
+    exec = require("child_process").exec;
+    fs = require("fs");
+    modules = {};
+
+    /**
+     * Sort object items by key.
+     *
+     * @method sortObject
+     * @private
+     * @param o {Object} The input object.
+     * @return {Object} The sorted object.
+     */
+    sortObject = function (o) {
+        var sorted = {}, key, a = [];
+        for (key in o) {
+            if (o.hasOwnProperty(key)) {
+                a.push(key);
+            }
+        }
+        a.sort();
+        for (key = 0; key < a.length; key++) {
+            sorted[a[key]] = o[a[key]];
+        }
+        return sorted;
+    };
+
+    // Add YUI as global variable to prevent undefined error.
     GLOBAL.YUI = function () {};
 
-    output["modules"] = {};
+    // Get module config by making a new YUI.add method.
     YUI.add = function (name, callback, version, config) {
-        if (config && config.requires) {
-            output["modules"][name] = {};
-            output["modules"][name].js = currentFile.replace(path, "");
-            output["modules"][name].requires = config.requires.sort();
-            if (config.css) {
-                output["modules"][name].css = config.css;
+        var i;
+        if (!config) {
+            return;
+        }
+        modules[name] = {};
+
+        // Append each property to the new object.
+        for (i in config) {
+            if (config.hasOwnProperty(i)) {
+                if (i === "requires") {
+                    // Beautifying the output.
+                    modules[name].requires = config.requires.sort();
+                } else {
+                    module[name][i] = config[i];
+                }
             }
-            if (config.group) {
-                output["modules"][name].group = config.group;
-            }
+        }
+
+        // Append file path.
+        if (!module[name].js) {
+            modules[name].js = currentFile.replace(path, "");
         }
     };
 
-    function sortObject(o) {
-        var sorted = {},
-        key, a = [];
-
-        for (key in o) {
-                if (o.hasOwnProperty(key)) {
-                                a.push(key);
-                        }
+    // Generate command.
+    cmd = ["find " + path + " -type f -name '*.js'"];
+    if (argv.ignore && argv.ignore.split(",").length) {
+        ignore = argv.ignore.split(",");
+        for (i in ignore) {
+            if (ignore.hasOwnProperty(i)) {
+                cmd.push("grep -v '" + ignore[i] + "'");
             }
-
-        a.sort();
-
-        for (key = 0; key < a.length; key++) {
-                sorted[a[key]] = o[a[key]];
-            }
-            return sorted;
+        }
     }
+    cmd.push("xargs grep 'YUI.add'");
+    cmd.push("cut -d: -f1");
 
-    // TODO - Add ignore parameter.
-    child = exec([
-        "find " + path + " -type f -name '*.js'",
-        "grep -v 'static/apps'",
-        "grep -v 'static/index/useradmin'",
-        "grep -v 'lib/miiicasa-sdk'",
-        "grep -v 'lib/yui'",
-        "grep -v 'lib/soundmanager'",
-        "grep -v 'lib/lazyload'",
-        "grep -v 'static/lab'",
-        "grep -v 'lang'",
-        "xargs grep 'YUI.add'",
-        "cut -d: -f1",
-        ].join("|"),
-        function (error, stdout, stderr) {
-            var fileList = stdout.split("\n");
-            for (var i in fileList) {
+    modules = {};
+    exec(cmd.join("|"), function (error, stdout, stderr) {
+        var fileList,
+            i,
+            output;
+        fileList = stdout.split("\n");
+        for (i in fileList) {
+            if (fileList.hasOwnProperty(i)) {
                 currentFile = fileList[i];
                 try {
                     require(fileList[i]);
-                } catch (e) {
-
-                }
+                } catch (e) {} // Ignore any errors.
             }
-            output = sortObject(output);
-            fs.writeFileSync("module.json", JSON.stringify(output, null, 4));
         }
-    );
+        modules = sortObject(modules);
+        output = argv.output;
+        if (output) {
+            fs.writeFileSync(output, JSON.stringify(modules, null, 4)); // 4 spaces.
+        } else {
+            console.log(modules);
+        }
+    });
 }());
